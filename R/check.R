be35d951c19c32c61c8313f4580d24a1d64324e0
Edwin de Jonge <edwindjonge@gmail.com>
2011-02-06 00:00:00 +0000

    version 0.4

--- R/check.R
+++ R/check.R
@@ -1,76 +1,154 @@
-#' Check if rows of a \code{data.frame} are valid according to the rules in \code{edtmatrix}
+#' Check rows of data.frame against edits.
 #'
-#' This function can be used to seperate the valid rows/observations from the invalid ones.
-#' This method will fail if \code{edtmatrix} contains variables that are not available in \code{dat}
+#' This is an S3 generic function for checking rows of a \code{data.frame} against
+#' a number of edit restrictions. The edits can be entered either in \code{character}
+#' \code{data.frame} or \code{editmatrix} format.
+#'
+#' If edits are represented as a \code{character} vector, the entries of \code{E} are parsed
+#' and evaluated in the environment of \code{dat}
+#'
+#' If the edits are  represented in a \code{data.frame}, the \code{data.frame} must have the format
+#' described in \code{\link{editmatrix}}. The edits are coerced to a character vector, and passed
+#' to \code{checkRows.character}.
+#'
+#' If the edits are represented by an \code{\link{editmatrix}} (representing linear (in)equalities)
+#' verbose edits are extracted and passed on to \code{checkRows.character}
+#'
+#'
+#' @aliases checkRows.character checkRows.data.frame checkRows.editmatrix
+#'
+#' @param E Edits, in \code{character}, \code{data.frame} or \code{\link{editmatrix}} representation
+#' @param dat The data to check.
+#' @return a logical vector with \code{length} equal to \code{nrow(dat)}. If a row is violates 
+#'      no edit restrictions, \code{TRUE} otherwise \code{FALSE}
+#'
+#' @seealso violatedEdits
 #' @example examples/checkRows.R
 #' @export
-#' @seealso errorMatrix
-#' @param edtmatrix \code{\link{editmatrix}} containing the constraints for \code{dat}
-#' @param dat \code{data.frame} with data that should be checked
-#' @return a logical vector with \code{length} equal to \code{nrow(dat)}. If a row is valid is \code{TRUE} otherwise \code{FALSE}
-checkRows <- function( edtmatrix
-                     , dat
-					      ){
-	stopifnot(is.editmatrix(edtmatrix), is.data.frame(dat))
-	vars <- colnames(edtmatrix) %in% names(dat)
-	if (!all(vars)){
-	   stop("Edits contain variable(s):", paste(colnames(edtmatrix)[!vars], collapse=","), ", that are not available in the data.frame")
-	}
-	
-	edts <- edits(edtmatrix)
-	check <- rep(TRUE, nrow(dat))	
-	for (i in seq(along.with=edts)){
-	   check <- check & eval(edts[[i]], envir=dat)
-	}
-    check
-	#TODO make a matrix an do the computation on the matrix.
+checkRows <- function(E, dat){
+    UseMethod("checkRows")
+}
+
+#' @nord
+#' @export
+checkRows.editmatrix <- function(E, dat){
+    stopifnot(is.data.frame(dat))
+    vars <- colnames(E) %in% names(dat)
+    if (!all(vars)){
+       stop("Edits contain variable(s):", paste(colnames(E)[!vars], collapse=","), 
+            ", that are not available in the data.frame")
+    }
+
+    return(checkRows.character(editrules(E)$edit, dat))
+    #TODO make a matrix an do the computation on the matrix.
 } 
 
-#' Check which rows of \code{data.frame dat} violate which constraints
+#' @nord
+#' @export
+checkRows.character <- function(E, dat){
+    
+    ed <- parseEdits(E)
+    check <- !logical(nrow(dat))
+    for ( i in 1:length(E)){
+        check <- check & tryCatch(eval(ed[[i]], envir=dat), error=function(e){
+            stop(paste("Edit",ed[[i]],"can not be checked. Evaluation returned",e$message,sep="\n" ))
+        })
+    }
+    return(check)
+}
+
+#' @nord
+#' @export
+checkRows.data.frame <- function(E, dat){
+    if ( !all(c("name","edit","description") %in% names(E)) ){
+        stop("Invalid input data.frame see ?editMatrix for valid input format")
+    }
+    return(checkRows.character(as.character(E$edit), dat))
+}
+
+
+#' Retrieve which rows of \code{data.frame dat} violate which constraints
+#'
+#' This is an S3 generic function for checking rows of a \code{data.frame} against
+#' a number of edit restrictions. The edits can be entered either in \code{character}
+#' \code{data.frame} or \code{editmatrix} format. The returned value is a logical matrix
+#' with dimension (number of records )\eqn{times}(number of edits), indicating which
+#' record violates (\code{TRUE}) which edit.
 #'
 #' This function can be used as an input for automatic corrections methods.
-#' This method will fail if \code{edtmatrix} contains variables that are not available in \code{dat}
-#' @example examples/errorMatrix.R
+#' This method will fail if \code{E} contains variables that are not available in \code{dat}
+#' 
+#' @aliases violatedEdits.character violatedEdits.data.frame violatedEdits.editmatrix
+#' @example examples/violatedEdits.R
 #' @export
-#' @seealso listErrors
-#' @param edtmatrix \code{\link{editmatrix}} containing the constraints for \code{dat}
+#' @seealso \code{\link{listViolatedEdits}}, \code{\link{checkRows}}
+#' @param E \code{\link{editmatrix}} containing the constraints for \code{dat}
 #' @param dat \code{data.frame} with data that should be checked
+#' @param ... further arguments that can be used by methods implementing this generic function
 #' @return a logical matrix where each row indicates which contraints are violated
-errorMatrix <- function( edtmatrix
-                       , dat
-                       ){
-	stopifnot(is.editmatrix(edtmatrix), is.data.frame(dat))
-	vars <- colnames(edtmatrix) %in% names(dat)
-	if (!all(vars)){
-	   stop("Edits contain variable(s):", paste(colnames(edtmatrix)[!vars], collapse=","), ", that are not available in the data.frame")
-	}
-	
-	edts <- edits(edtmatrix)
-	errors <- matrix( FALSE
-	                , ncol=length(edts)
-				    , nrow=nrow(dat)
-				    , dimnames=list(rownames(dat), rownames(edtmatrix))
-				    )
-	for (i in seq(along.with=edts)){
-	   errors[,i] <- !eval(edts[[i]], envir=dat)
-	}
-    errors
+violatedEdits <- function(E, dat, ...){
+    UseMethod("violatedEdits")
 }
+
+
+#' @nord
+#' @export
+violatedEdits.character <- function(E, dat, name=NULL, ...){
+    ed <- parseEdits(E)
+    M <- tryCatch(sapply(ed, eval, envir=dat), error=function(e){
+        stop(paste("Not all edits can be evaluated, parser returned", e$message, sep="\n"))})
+    colnames(M) <- name
+    return(!M)
+}
+
+#' @nord
+#' @export
+violatedEdits.editmatrix <- function(E, dat, ...){
+    er <- editrules(E)
+    return(violatedEdits.character(er$edit, dat, er$name))
+}
+
+#' @nord
+#' @export
+violatedEdits.data.frame <- function(E, dat, ...){
+    if ( !all(c("name","edit","description") %in% names(E)) ){
+        stop("Invalid input data.frame see ?editmatrix for valid input format")
+    }
+    return(violatedEdits.character(as.character(E$edit), dat, E$name))
+}
+
 
 #' Lists which rows of \code{data.frame dat} violate which constraints
 #'
 #' This function can be used as an input for automatic corrections methods.
-#' @example examples/listErrors.R
-#' @seealso errorMatrix
+#' @example examples/listViolatedEdits.R
 #' @export
-#' @param edtmatrix \code{\link{editmatrix}} containing the constraints for \code{dat}
+#' @param E a number of edit restrictions, represented as \code{character} vector, \code{\link{editmatrix}} or \code{data.frame}.
 #' @param dat \code{data.frame} with data that should be checked
+#' @seealso \code{\link{violatedEdits}} \code{\link{checkRows}}
 #' @return a list with per row a \code{integer} vector of the constraints that are violated 
-listErrors <- function( edtmatrix
-                      , dat
-                      ){	
-	errors <- errorMatrix(edtmatrix, dat)
-	edts <- edits(edtmatrix)
-	errorlist <- apply(errors, 1, which)
-	errorlist
+listViolatedEdits <- function(E, dat){    
+    errors <- violatedEdits(E, dat)
+    errorlist <- apply(errors, 1, which)
+    return(apply(errors, 1, which))
+}
+
+
+#' Parse a character vector of edits
+#'
+#' This function wraps the native \code{\link{parse}} function in a \code{\link{tryCatch}}.
+#' The function is \code{editrules} internal. It tries to give a meaningfull error message when
+#' parsing fails for some reason.
+#'
+#' @param E \code{character}
+#' @return The edits in \code{E} parsed to R expressions.
+#'
+parseEdits <- function(E){
+     return(
+        tryCatch(parse(text=E), 
+            error=function(e){
+                stop(paste("Not all edits can be parsed, parser returned", e$message,sep="\n"))
+            }
+        )
+    )
 }
--- R/editmatrix.R
+++ R/editmatrix.R
@@ -1,67 +1,98 @@
-retrieveSign <- function(e, fac=1){
+COPS <- c("==","<","<=",">",">=")
+
+retrieveCoef <- function(e, co=1){
    #stopifnot(is.language(e))
    if (length(e) == 1){
      if (is.numeric(e)){
-        l <- fac*e
+        l <- co*e
         names(l) <- getOption("editrules.CONSTANT", "CONSTANT")
      }
      else {
-        l <- fac
+        l <- co
         names(l) <- as.character(e)
      }
-	  return(l)
+      return(l)
    }
    if (length(e) == 2){
-      op <- deparse(e[[1]])
-	  rhs <- e[[2]]
-      if (op == "("){
-	    return(retrieveSign(rhs, fac))
+     op <- deparse(e[[1]])
+      rhs <- e[[2]]
+     if (op == "("){
+	    return(retrieveCoef(rhs, co))
 	  } 
+     else if (op == "-"){
+        return(retrieveCoef(rhs, -1*co))
+     }
+	  else { 
+		stop("Operator ", op, " not implemented", "Invalid expression:", e)
+	  }
    }
    if (length(e) == 3){
-      op <- deparse(e[[1]])
-	  lhs <- e[[2]]
-	  rhs <- e[[3]]
-	  lsign <- rsign <- fac
-      if ( op == "=="
-	    || op == "<"
-	    || op == "=<"
-	    || op == "-"
-		 ){
-	    rsign <- -1 * fac
+     op <- deparse(e[[1]])
+      lhs <- e[[2]]
+      rhs <- e[[3]]
+      lsign <- rsign <- co
+     if ( op %in% c(COPS, "-")){
+	    rsign <- -1 * co
 	  } 
-	  else if ( op == ">"
-	         || op == ">="
-		      ){
-	    lsign <- -1 * fac
-	  }
 	  else if (op == "+"){
 	  }
 	  else if (op == "*"){
-	    #TODO check if lhs is a numeric
-	    return(retrieveSign(rhs, eval(lhs)*fac))
+       co <- retrieveCoef(lhs, co)
+       if (!is.numeric(co)){
+                stop(paste("Expression contains nonconstant coefficient", paste(lhs,collapse="")))
+       }
+       return(retrieveCoef(rhs, co))
 	  }
 	  else { 
-		stop(". Operator ", op, " not implemented", "Invalid expression:", e)
+		stop("Operator ", op, " not implemented", "Invalid expression:", e)
 	  }
-	  return(c( retrieveSign(lhs, lsign)
-		      , retrieveSign(rhs, rsign)
+	  return(c( retrieveCoef(lhs, lsign)
+		      , retrieveCoef(rhs, rsign)
 		  )
 		)
    }
    stop("Invalid expression:", e)
 }
 
+parseGuard <- function(g){
+  op <- as.character(g[[1]])
+  if (op %in% c( COPS
+               , "||"
+               , "&&"
+               ,"%in%"
+               )
+     ){
+  }
+  else {
+     stop("Invalid condition syntax: ", g)
+  }
+}
+
 makeEditRow <- function(edt){
-  if (length(edt) != 3){
+  op <- as.character(edt[[1]])
+  if (op == "if"){
+     stop("Conditional edit rules are not (yet) supported.", edt)
+     guard <- edt[[2]]
+     parseGuard(guard)
+     edt <- edt[[3]]
+     op <- as.character(edt[[1]])  
+  }
+  if (!(op %in% COPS)){
      stop(paste("Invalid edit rule:", edt))
   }
-  wgt  <- retrieveSign(edt)
-  stopifnot(length(wgt)==length(unique(names(wgt))))
+  wgt <- retrieveCoef(edt)
+  # simplify the coefficients by summing them
+  wgt <- tapply(wgt, names(wgt), sum)
   return(wgt)  
 }
 
-#' Transforms a list of R (in)equalities into an edit matrix with a factor for each variable
+#' Transforms a list of R (in)equalities into an edit matrix.
+#'
+#' Transforms a list of R (in)equalities into an edit matrix with coefficients for each variable, and a constant (\code{C})
+#' and operator (\code{ops}) for each edit rule.
+#'
+#' Each row in the resulting editmatrix represents an linear (in) equality.
+#' Each column in the resulting editmatrix represents a variable.
 #'
 #' There are two forms of creating an editmatrix:
 #' \enumerate{ 
@@ -80,67 +111,117 @@ makeEditRow <- function(edt){
 #' If the first form is used, \code{editmatrix} internally creates the second form. This information
 #' can be retrieved by using \code{\link{editrules}}
 #'
-#' The matrix is created by getting the factors of the variables in the equalities.
-#' i.e. \code{x == y}   results in  \code{c(x=-1, y=1, w=0, z=0)}
-#' and \code{x == y + w} results in \code{c(x=-1, y=1, w=1, z=0)}
-#' @title Reading in edit rules
+#' The matrix is created by retrieving the coefficients of the variables in the equalities.
+#' i.e. \code{x == y}   results in  \code{c(x=1, y=-1)}
+#' and \code{x == y + w} results in \code{c(x=1, y=-1, w=-1)}
+#'
+#' By default the editmatrix is created using the comparison operators (\code{==,<=,>=,<,>}) in the edits. If option \code{normalize=TRUE} is used all 
+#' edits are transformed into an E == C, E < C or E <= C form, so that in the specification of the edit rules all inequalities can be mixed, 
+#' but the resulting matrix has similar sign.
+#' @title Create an editmatrix
 #' @seealso \code{\link{editrules}} \code{\link{as.editmatrix}}
 #' @export
 #' @example examples/editmatrix.R
 #'
 #' @param editrules \code{data.frame} with (in)equalities written in R syntax, see details for description or alternatively 
 #'        a \code{character} with (in)equalities written in R syntax
-#' @param editsinfo deprecated
+#' @param normalize \code{logical} specifying if all edits should be transformed (see description)
 #'
 #' @return an object of class "editmatrix" which is a \code{matrix} with extra attributes
-editmatrix <- function( editrules = editsinfo
-					       , editsinfo = NULL
-					       ){
-   if (!missing(editsinfo)){
-      warning("this parameter is deprecated, please use parameter editrules")
-   }
-   
+editmatrix <- function( editrules
+                      , normalize = FALSE
+					       ){   
    if (is.character(editrules)){
-	   edts <- parse(text=editrules)
-	   editrules <- data.frame(edit=sapply(edts, deparse))
-	}
-	else if (is.data.frame(editrules)){
-	   edts <- parse(text=editrules$edit)
-	   editrules$edit <- sapply(edts, deparse)
-	}
+      edit <- editrules
+      name <- NULL
+      description <- NULL
+      editrules <- NULL
+    }
+    else if (is.data.frame(editrules)){
+      name <- editrules$name
+      edit <- editrules$edit
+      description <- editrules$description
+
+      if (is.null(edit)){
+         stop("The supplied data.frame misses the column 'edit'.\nSee ?editmatrix for a valid input specification")
+      }      
+      
+      editrules$name <- NULL
+      editrules$edit <- NULL
+      editrules$description <- NULL      
+    }
    else {
-      stop("Invalid input")
+      stop("Invalid input, please use a character vector or a data.frame.\n See ?editmatrix for a valid input specification")
    }
 
-	if (is.null(editrules$name)){
-	   editrules$name <- paste("rule", seq(along.with=edts),sep=" ")
+   edts <- tryCatch(parse(text=edit), error=function(e){
+         stop(paste("The edits could not be parsed. Parser returned\n",e$message))})   
+   stopifnot(is.language(edts))
+   
+   edit <- sapply(edts, deparse)
+   edit <- gsub(" * ","*", fixed=TRUE, edit)
+   
+	if (is.null(name)){
+	   name <- paste("e", seq_along(edit),sep="")
 	}
 	
-	if (is.null(editrules$description)){
-	   editrules$description <- rep("", length(edts))
+	if (is.null(description)){
+	   description <- rep("", length(edit))
 	}
-	editrules <- editrules[c("name","edit","description")]
-
-	stopifnot(is.language(edts))
+   
+   # create/update the name, edit, description vectors and keep the original other vectors
+   editrules <- data.frame( name=name,edit=edit,description=description
+                          , stringsAsFactors=FALSE
+                          )
     
-    rowedts <- lapply(edts, function(edt){makeEditRow(edt)})
-	vars <- unique(names(unlist(rowedts)))
+   rowedts <- lapply(edts, function(edt){makeEditRow(edt)})
+   ops <- sapply(edts, function(e){deparse(e[[1]])})
+   C <- numeric(length(ops))
+   
+    vars <- unique(names(unlist(rowedts)))
 
-	mat <- matrix( 0
-	             , ncol=length(vars)
+    mat <- matrix( 0
+                 , ncol=length(vars)
                 , nrow=length(rowedts)
                 , dimnames = list( rules = editrules$name
                                  , var=vars
                                  )
                 )
-
-	for (i in 1:length(rowedts)){
-	   mat[i,names(rowedts[[i]])] <- rowedts[[i]]
+    for (i in 1:length(rowedts)){
+       mat[i,names(rowedts[[i]])] <- rowedts[[i]]
    }
-	structure( mat
+   
+   if (normalize){ 
+      for (i in 1:nrow(mat)){
+         if (ops[i] == ">="){
+            mat[i,] <- -mat[i,]
+            ops[i] <- "<"
+         }
+         else if (ops[i] == ">"){
+            mat[i,] <- -mat[i,]
+            ops[i] <- "<="
+         }
+      }
+   }
+
+   if ((m  <- match("CONSTANT", colnames(mat), nomatch=0))){
+      C <- -1*mat[,m]
+      mat <- mat[,-m, drop=FALSE]
+   }
+   
+   if (normalize){
+      return(as.editmatrix(mat,C=C, ops=ops, normalize=FALSE))
+   }
+
+	names(ops) <- name
+   names(C) <- name
+   
+   structure( mat
 	         , class="editmatrix"
             , editrules=editrules
             , edits = edts
+            , ops = ops
+            , C = C
             )
 }
 
@@ -159,32 +240,83 @@ edits <- function(x){
    return(attr(x, "edits"))
 }
 
-#' Transform a matrix into an edit matrix
+#' Coerce to an edit matrix. This method will derive editrules from a matrix.
 #'
+#' \code{as.editmatrix} interpretes the matrix as an editmatrix and derives readable edit rules. 
 #' The columns of the matrix
 #' are the variables and the rows are the edit rules (contraints).
+#' 
+#' If only argument \code{x} is given (the default), the resulting editmatrix is of the form \eqn{Ex=0}. 
+#' This can be influenced by using the parameters \code{C} and \code{ops}.
 #'
 #' @export
 #' @seealso \code{\link{editmatrix}}
 #'
-#' @param x object to be transformed into an \code{\link{editmatrix}}
+#' @param x object to be transformed into an \code{\link{editmatrix}}. \code{x} will be coerced to a matrix.
+#' @param C Constant, a \code{numeric} of \code{length(nrow(x))}, defaults to 0
+#' @param ops Operators, \code{character} of \code{length(nrow(x))} with the equality operators, defaults to "=="
+#' @param ... further parameters will be given to \code{editmatrix}
 #'
 #' @return an object of class \code{editmatrix}.
-as.editmatrix <- function(x){
+as.editmatrix <- function( x
+                         , C = numeric(nrow(mat))
+                         , ops = rep("==", nrow(mat))
+                         , ...
+                         ){
    if (is.editmatrix(x)){
       return(x)
    }
    mat <- as.matrix(x)
-   structure( mat
-            , class="editmatrix"
-			   , editrules=editrules(mat)
-			   )
+  
+   vars <- colnames(mat)
+   if (is.null(vars)){
+       if ((n <- ncol(mat)) > length(letters)){
+		   vars <- character(n)
+		   vars[1:n] <- letters
+	   } else{
+	      vars <- letters[1:n]
+	   }
+   }
+   colnames(mat) <- make.names(vars, unique=TRUE)
+   
+   #print(mat)
+   nC <- ncol(mat) + 1
+   er <- character(nrow(mat))
+   for (i in 1:nrow(mat)){
+     r <- c(mat[i,], -C[i])
+	  lhs <- r > 0
+	  rhs <- r < 0
+     
+	  r <- abs(r)
+	  
+	  facs <- paste(r, "*", vars, sep="")
+     
+	  facs[r==1] <- vars[r==1] #simplify 1's
+     
+	  facs[r==0] <- "" #remove 0's
+	  
+     #replace constant term
+     facs[nC] <- C[i]
+     
+	  leftterm <- if (any(lhs)) paste(facs[lhs], collapse=' + ')
+	              else 0
+				  
+	  rightterm <- if (any(rhs)) paste(facs[rhs], collapse=' + ')
+	               else 0
+	  er[i] <- paste(leftterm, ops[i], rightterm)
+   }
+   ei <- data.frame(edit=er)
+   ei$edit <- er
+   ei$name <- rownames(er)
+   editmatrix(er,...)
 }
 
-#' Convert an editmatrix to a normal matrix
+#' Coerce an editmatrix to a normal matrix
 #' 
 #' An \code{editmatrix} is a matrix and can be used as such, but it has extra attributes.
-#' In some case it is preferable to convert the editmatrix to a normal matrix.
+#' In some cases it is preferable to convert the editmatrix to a normal matrix.
+#
+#' Please note that coercion only returns the coefficient matrix part of the editmatrix, not the \code{C} or \code{ops} part.
 #'
 #' @export
 #' @method as.matrix editmatrix
@@ -192,9 +324,29 @@ as.editmatrix <- function(x){
 #' @param x editmatrix object
 #' @param ... further arguments passed to or from other methods.
 #'
-#' @return matrix equal to editmatrix
+#' @return coefficient matrix of editmatrix
 as.matrix.editmatrix <- function(x, ...){
    array(x, dim=dim(x), dimnames=dimnames(x))
+}
+
+#' Coerce an editmatrix to a \code{data.frame}
+#'
+#' Coerces an editmatrix to a \code{data.frame}. Useful for storing/viewing the matrix representation of editrules.
+#' @export 
+#' @method as.data.frame editmatrix
+#' @param x editmatrix object
+#' @param ... further arguments passed to or from other methods.
+#'
+#' @return data.frame with the coefficient matrix representation of \code{x}, a C column and a operator column.
+as.data.frame.editmatrix <- function(x, ...){
+   dat <- as.data.frame(as.matrix(x))
+   nms <- make.names( c(names(dat), "C", "Ops")
+                    , unique=TRUE
+                    )
+   n <- length(nms)
+   dat[[nms[n-1]]] <- getC(x)
+   dat[[nms[n]]] <- getOps(x)
+   dat
 }
 
 #' print edit matrix
@@ -206,7 +358,9 @@ as.matrix.editmatrix <- function(x, ...){
 #' @param ... further arguments passed to or from other methods.
 print.editmatrix <- function(x, ...){
    cat("Edit matrix:\n")
-   print(as.matrix(x,...))
+   m <- as.matrix(x)
+   m <- cbind(m, CONSTANT=getC(x))
+   print(m, ...)
    cat("\nEdit rules:\n")
    info <- editrules(x)
    desc <- paste("[",info$description,"]")
@@ -215,3 +369,7 @@ print.editmatrix <- function(x, ...){
       , "\n"
       )
 }
+
+
+
+
